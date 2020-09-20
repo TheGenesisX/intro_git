f65065806767978c680277c9f9e17f3acddf8d3c
Alejandro Silva <jose.silva5468@alumnos.udg.mx>
2020-09-19 19:48:15 -0500

    Captura de nombre completo e impresion de saludo con fgets

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(){
+    char nombre[100];
+
     printf("Hello World!\n");
+    printf("Ingresa tu nombre: ");
+    fgets(nombre, sizeof(nombre), stdin);
+    printf("Hola %s\n", nombre);
 
 return 0;
 }

ced34e949dc6cf9e8b6c448216355b35248b961c
Alejandro Silva <jose.silva5468@alumnos.udg.mx>
2020-09-19 20:09:35 -0500

    Captura de edad e impresion de edad + 1

--- main.c
+++ main.c
@@ -2,11 +2,15 @@
 
 int main(){
     char nombre[100];
+    unsigned int edad;
 
     printf("Hello World!\n");
     printf("Ingresa tu nombre: ");
     fgets(nombre, sizeof(nombre), stdin);
-    printf("Hola %s\n", nombre);
+    printf("Hola %s", nombre);
+    printf("Ingresa tu edad: ");
+    scanf("%u", &edad);
+    printf("Vas a cumplir: %u\n", edad + 1);
 
 return 0;
 }
